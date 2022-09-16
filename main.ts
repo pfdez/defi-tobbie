f89bd82962f67029b7c2b188716fcbff727d42dc
pfdez <31733411+pfdez@users.noreply.github.com>
2022-09-16 15:35:30 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+music.playMelody("C D E C C D E C ", 120)
+music.playMelody("E F G - E F G - ", 120)
+music.playMelody("G A G F E - C - ", 240)
+music.playMelody("G A G F E - C - ", 240)
+basic.forever(function () {
+    TobbieII.dance(100)
+})
